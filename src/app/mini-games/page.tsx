e1b929bbcbcff3b6b351a51464be22a3d38390cc
AstafievAndrey <carrats@yandex.ru>
2024-11-02 00:28:24 +0300

    list

---
+++ src/app/mini-games/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./mini-games.module.css";
+
+export default function MiniGames() {
+  return (
+    <section>
+      <ul className={styles.list}>
+        <li>Find a couple</li>
+      </ul>
+    </section>
+  );
+}
